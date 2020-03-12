587d4693b4794df77b6460271628ae80cc43b2e9
yangfuyuan <chushuifurong618@126.com>
2020-03-12 16:25:21 +0800

    等待包超时时，返回有效点数

--- sdk/src/ydlidar_driver.cpp
+++ sdk/src/ydlidar_driver.cpp
@@ -854,6 +854,7 @@ result_t YDlidarDriver::waitScanData(node_info *nodebuffer, size_t &count,
     ans = waitPackage(&node, timeout - waitTime);
 
     if (!IS_OK(ans)) {
+      count = recvNodeCount;
       return ans;
     }
 
